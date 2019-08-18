9c589784a8f1e9dcd1175b3cf2d42949a0760f64
Red Tail <redtail814@gmail.com>
2019-08-18 00:16:40 -0400

    Added jquery post function on survey page and added compatibility logic to API post request.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,5 +12,43 @@ module.exports = app => {
     // api POST requests
     app.post("/api/friends", (req, res) => {
         // will be used to handle incoming survey results and the compatibility logic
+
+        let match = {
+            name: "",
+            photo: "",
+            difference: 1000
+        };
+
+        console.log(req.body);
+
+        let data = req.body;
+        let userScores = data.scores;
+
+        console.log(userScores);
+
+        let totalDifference = 0;
+
+        for (var i = 0; i < friendData.length; i++) {
+
+            console.log(friendData[i]);
+            totalDifference = 0;
+
+            for (var j = 0; j < friendData[i].scores[j]; j++) {
+
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendData[i].scores[j]));
+
+                if (totalDifference <= match.difference) {
+
+                    match.name = friendData[i].name;
+                    match.photo = friendData[i].photo;
+                    match.difference = totalDifference;
+                }
+            }
+        }
+
+        friendData.push(data);
+
+        res.json(match);
+
     });
 };
