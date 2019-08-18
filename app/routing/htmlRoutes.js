cccde1a288bffbdece5d83faca39e0a6d4cb5d97
Red Tail <redtail814@gmail.com>
2019-08-17 22:45:31 -0400

    Added survey and logo.  Styled html pages.  Adjusted html routes to match new folder structure.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,17 +5,17 @@ module.exports = app => {
 
     //  home.html route
     app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/assets/view/home.html"));
     });
 
     //  survey.html route
     app.get("/survey", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/assets/view/survey.html"));
     });
 
     // if no matching route is found default to home
     app.use((req, res) => {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/assets/view/home.html"));
     });
 
 };
